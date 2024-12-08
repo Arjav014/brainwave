80cdc8bf194f8e778693fb97e657e0c33f15e4a1
Arjav014 <arjav2768@gmail.com>
2024-12-08 23:46:04 +0530

    Created basic section structure

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -13,13 +13,13 @@ const Button = ({ className, href, onClick, children, px, white }) => {
       {ButtonSvg(white)}
     </button>
   );
- 
+
   const renderLink = () => (
     <a href={href} className={classes}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </a>
-  )
+  );
 
   return href ? renderLink() : renderButton();
 };
